c3e31059fe9cc701cf00429b61d85c777b714f9a
alessandrocastelli2005 <alessandro8.castelli@mail.polimi.it>
2024-11-25 23:20:01 +0100

    csv + sketch

--- sketch.js
+++ sketch.js
@@ -1,17 +1,82 @@
+let data;
+let barColors = []; 
+
 function preload() {
-  // put preload code here
+ 
+  data = loadTable("river_lengths.CSV", "csv", "header");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  background(240);
+
+  let names = data.getColumn("name");
+  let lengths = data.getColumn("length").map(Number);
+
+
+  let maxLength = 6650; 
+
+
+  let margin = 80;
+  let barWidth = (width - margin * 2) / names.length;
+
+
+  for (let i = 0; i < names.length; i++) {
+    barColors.push(color(random(100, 255), random(100, 255), random(100, 255)));
+  }
+
+  stroke(0);
+  line(margin, height - margin, width - margin, height - margin); 
+  line(margin, margin / 2, margin, height - margin); 
+
+  textAlign(RIGHT);
+  textSize(12);
+  fill(0);
+
+  let step = 1000; 
+  for (let i = 0; i <= maxLength; i += step) {
+    let y = map(i, 0, maxLength, height - margin, margin / 2); 
+    line(margin - 5, y, margin, y); 
+    text(i + " km", margin - 10, y + 5); 
+  }
+
+  textAlign(CENTER);
+  textSize(20);
+  text("Lunghezza dei principali fiumi del mondo", width / 2, margin / 2);
+
+  let selectedRiver = "";
+
+  for (let i = 0; i < names.length; i++) {
+    let x = margin + i * barWidth;
+    let barHeight = map(lengths[i], 0, maxLength, 0, height - margin * 1.5);
+
+    fill(barColors[i]);
+    noStroke();
+    rect(x, height - margin - barHeight, barWidth, barHeight);
+
+    if (
+      mouseX > x &&
+      mouseX < x + barWidth &&
+      mouseY > height - margin - barHeight &&
+      mouseY < height - margin
+    ) {
+      selectedRiver = `${names[i]}: ${lengths[i]} km`;
+    }
+  }
+
+  if (selectedRiver !== "") {
+    fill(0);
+    textSize(16);
+    text(selectedRiver, width / 2, height - 30);
+  }
 }
 
 function draw() {
-  // put drawing code here
+  background(240);
+
+  setup();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
